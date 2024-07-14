a2ca2ab0af22d2895afb799106116760d46ad191
Prashik Jadhav <prashikjadhav17@gmail.com>
2024-07-15 01:12:51 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function scrollskill(){
+    var ele = document.getElementById("skills");
+    ele.scrollIntoView()
+}
+
+function scrollhome(){
+    var ele = document.getElementById("home");
+    ele.scrollIntoView()
+}
+function scrollcon(){
+    var ele = document.getElementById("contact");
+    ele.scrollIntoView()
+}
+
